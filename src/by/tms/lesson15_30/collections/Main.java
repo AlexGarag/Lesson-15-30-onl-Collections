07cde04a5baa5375a09bc9342d20f5fef5bdae27
alex garag <alex.garag@bk.ru>
2024-07-23 19:31:29 +0300

    задача 1 готова

---
+++ src/by/tms/lesson15_30/collections/Main.java
@@ -0,0 +1,7 @@
+package by.tms.lesson15_30.collections;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/by/tms/lesson15_30/collections/tasks/Task1.java
@@ -0,0 +1,30 @@
+package by.tms.lesson15_30.collections.tasks;
+
+/*
+    Пользователь вводит набор чисел в виде одной строки с клавиатуры. Например: "1, 2, 3, 4, 4, 5".
+    Избавиться от повторяющихся элементов в строке. Вывести результат на экран.
+    При решении использовать коллекции.
+*/
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Set<Integer> treeSet = new TreeSet<>();
+        Scanner sc = new Scanner(System.in);
+        int num;
+        while (true) {
+            System.out.print("Input a number: ");
+            num = sc.nextInt();
+            if (num < 0) {
+                break;
+            }
+            treeSet.add(num);
+        }
+        for (Integer integer : treeSet) {
+            System.out.println(integer);
+        }
+    }
+}
